23e458cb6488f9079322f1c9ee79d406f219233a
MaximeS <maxime.simet@yahoo.fr>
2016-04-22 14:52:36 +0200

    Page d'accueil terminee, prête a être designee

--- index.php
+++ index.php
@@ -7,31 +7,26 @@ Autoloader::register();
 <!doctype html>
 <head>
     <title> Station Météo</title>
-    
+    <!-- Jquery -->
     <script type="text/javascript" src="js/libs/jquery-2.2.3.min.js"></script>
-    
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
     <!-- Highcharts -->
     <script src="js/libs/highcharts.js"></script>
     <script src="js/libs/highcharts-more.js"></script>
     <script src="js/libs/exporting.js"></script>
-
     <!-- Fonctions graphiques et JSON -->
     <script type="text/javascript" src="js/graphiqueJauge.js"></script>
      <script type="text/javascript" src="js/graphiqueSolidJauge.js"></script>
     <script type="text/javascript" src="js/graphiqueCourbe.js"></script>
     <script type="text/javascript" src="js/graphiqueStyles.js"></script>
     <script type="text/javascript" src="js/jsonHandler.js"></script>
-    
     <script type="text/javascript" src="js/graphiqueOptions"></script>
     <script type="text/javascript" src="js/main.js"></script>
-    
     <script type="text/javascript" src="js/progressBarr.js"></script>
-    
     <script type="text/javascript" src="js/ajax.js"></script>
 </head>
 
@@ -43,29 +38,47 @@ Autoloader::register();
         <div class="jauge col-xs-2" id="humidite"></div>
         <div class="jauge col-xs-2" id="pluie"></div>
         <div class="jauge col-xs-2" id="pression"></div>
-       
         <div class="jauge col-xs-2" id="luminosite"></div>
-        
-    
+    </div>
+    <div class="row">
+        <div class="jauge col-xs-2" id="temperatureExterieure"></div>
+        <div class="solidJauge col-xs-6" id="deltaTemperature" ></div>
+                <div class="col-xs-2">
+            <?php
+            $Sat24 = new Sat24();
+            echo $Sat24->afficheLune();
+            
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-8">
+            <?php
+            echo $Sat24->Date();
+            ?>
+        </div>
+        <div class="col-xs-2"><h4>Interprétation :</h4></br>
+            Alertes : </br>
+            Humidité : 100%  : Fort risque de pluie </br>
+            Pression : Baisse de 2HPa dans les 2 dernières heures ! </br>        
+        </div>
     </div>
 </div>
 
 
+    <script src="js/libs/solid-jauge.js"></script>
 
-<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
 
-<div style="width: 600px; height: 400px; margin: 0 auto">
-     <div class="jauge col-xs-2" id="temperatureExterieure"></div>
-    <div id="deltaTemperature" style="width: 300px; height: 200px; float: left"></div>
-</div>
 
-<!--col-xs-4 col-sm-3 col-md-2-->
+
+
+<!--col-xs-4 col-sm-3 col-md-2
  Affichage des graphiques journaliers
  <div class="graphique" id="grapheHumidite" ></div>
 <div class="graphique" id="grapheTemperatureExterieure" ></div>
 <div class="graphique" id="grapheDetectionEau"></div>
 <div class="graphique" id="grapheLuminosite"></div>
-
+-->
 
 </body>
 
