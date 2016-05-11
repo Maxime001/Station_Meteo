2bfbc79647a3b48b0982ff79a7e889920cf58660
Maxime001 <maxime.simet@yahoo.fr>
2016-05-11 13:57:50 +0200

    mise a jour class bdd

--- class/BaseDonnees.php
+++ class/BaseDonnees.php
@@ -1,15 +1,6 @@
 <?php
 
-class BaseDonnees {
-    private $bdd="";
-    
-    /**
-     * Connection a la base de donnée
-     */
-    public function __construct(){
-        try{ $this->bdd = new PDO('mysql:host=localhost;dbname=meteo;charset=utf8', 'root', 'P3gaze1992'); }
-        catch(Exception $e){die('Erreur : '.$e->getMessage());}
-    }
+class BaseDonnees extends Bdd {
     
     public function recup24h(){
         date_default_timezone_set("Europe/Paris");
