b6a01e3ef4157c1c7c83a9bce6db649628dcd215
Maxime001 <maxime.simet@yahoo.fr>
2016-05-11 13:40:04 +0200

    test git

--- class/BaseDonnees.php
+++ class/BaseDonnees.php
@@ -7,7 +7,7 @@ class BaseDonnees {
      * Connection a la base de donnée
      */
     public function __construct(){
-        try{ $this->bdd = new PDO('mysql:host=localhost;dbname=meteo;charset=utf8', 'root', ''); }
+        try{ $this->bdd = new PDO('mysql:host=localhost;dbname=meteo;charset=utf8', 'root', 'P3gaze1992'); }
         catch(Exception $e){die('Erreur : '.$e->getMessage());}
     }
     
