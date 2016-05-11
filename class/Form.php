5762850261a580b008aea4407cef06757a2fd400
MaximeS <maxime.simet@yahoo.fr>
2016-05-11 17:13:52 +0200

    mise en place de la page de connexion

---
+++ class/Form.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of form
+ *
+ * @author Max
+ */
+class Form {
+     /**
+     * @var array Données utilisées par le formualaire
+     * */
+    protected $data;
+    /*
+     *
+     */
+    public $surround = 'p';
+    /*
+     * 
+     */
+    public function __construct($data = array()){
+        $this->data = $data;
+    }
+    /*
+     * @param $html string Code HTML à entourer
+     * @return string
+     */
+    protected function surround($html){
+        return "<{$this->surround}>{$html}</{$this->surround}>";
+    }
+    /*
+     *
+     */
+    protected function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index] : null;
+    }
+    /*
+     *
+     */
+    public function input($name,$type){
+         return $this->surround(
+             '<input type="'.$type.'" name="' . $name . '"value = "' . $this->getValue($name) . '">');
+    }
+    /*
+     *
+     */
+    public function submit(){
+        return $this->surround('<button type="submit">Envoyer</button>');
+    }
+}
