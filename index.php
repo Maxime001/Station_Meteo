66ce6567fc9d65469b7740b3f999deea075ae8e4
MaximeS <maxime.simet@yahoo.fr>
2016-05-14 21:11:33 +0200

    debug

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     Autoloader::register();  
     
 
-  //  $ip = new BloquageIp();
-  //  $res = $ip->verifBloque();
+    $ip = new BloquageIp();
+    $res = $ip->verifBloque();
     
- //   if($res == true){
- //       include '404.php';
- //   }
- //   else{    
+    if($res == true){
+        include '404.php';
+    }
+    else{    
         if(!isset($_SESSION['validUser'])){
             include 'verif.php';
         }
