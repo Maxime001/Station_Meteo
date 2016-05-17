eed5c59d2e93da3dbd6ea35655492e4508d9ed19
MaximeS <maxime.simet@yahoo.fr>
2016-05-17 14:40:08 +0200

    meteo

--- main.php
+++ main.php
@@ -1,113 +1,116 @@
-<?php
-
-if($_SERVER['REQUEST_URI'] == '/main.php') header('Location:/');
-?><!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <title> Station Météo</title>
-    <!-- Jquery -->
-    <script type="text/javascript" src="js/libs/jquery-2.2.3.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <!-- Highcharts -->
-    <script src="js/libs/highcharts.js"></script>
-    <script src="js/libs/highcharts-more.js"></script>
-    <script src="js/libs/exporting.js"></script>
-    <!-- Fonctions graphiques et JSON -->
-    <script type="text/javascript" src="js/graphiqueJauge.js"></script>
-    <script type="text/javascript" src="js/graphiqueSolidJauge.js"></script>
-    <script type="text/javascript" src="js/graphiqueCourbe.js"></script>
-    <script type="text/javascript" src="js/graphiqueStyles.js"></script>
-    <script type="text/javascript" src="js/jsonHandler.js"></script>
-    <script type="text/javascript" src="js/graphiqueOptions"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-    <script type="text/javascript" src="js/progressBarr.js"></script>
-    <script type="text/javascript" src="js/ajax.js"></script>
-    <script src="js/libs/solid-jauge.js"></script>
-    <script type="text/javascript"></script>
-    
+    <head>
+        <title> Station Météo</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="shortcut icon" href="../favicon.ico">
+        <!-- CSS du template -->
+        <link rel="stylesheet" type="text/css" href="template/css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="template/css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="template/css/component.css" />
+        <!-- JS du template -->
+        <script src="js/modernizr.custom.js"></script>        
+        <!-- Jquery -->
+        <script type="text/javascript" src="js/libs/jquery-2.2.3.min.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/contenu.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <!-- Highcharts -->
+        <script src="js/libs/highcharts.js"></script>
+        <script src="js/libs/highcharts-more.js"></script>
+        <script src="js/libs/exporting.js"></script>
+        <!-- Fonctions graphiques et JSON -->
+        <script type="text/javascript" src="js/graphiqueJauge.js"></script>
+        <script type="text/javascript" src="js/graphiqueSolidJauge.js"></script>
+        <script type="text/javascript" src="js/graphiqueCourbe.js"></script>
+        <script type="text/javascript" src="js/graphiqueStyles.js"></script>
+        <script type="text/javascript" src="js/jsonHandler.js"></script>
+        <script type="text/javascript" src="js/graphiqueOptions"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/progressBarr.js"></script>
+        <script type="text/javascript" src="js/ajax.js"></script>
+        <script src="js/libs/solid-jauge.js"></script>
+        <script type="text/javascript"></script>
+    </head>
+    <body class="main">
+        <div class="container-fluid">
+            <ul id="gn-menu" class="gn-menu-main">
+                <li class="gn-trigger">
+                    <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+                    <nav class="gn-menu-wrapper">
+                        <div class="gn-scroller">
+                            <ul class="gn-menu">
+                                <li class="gn-search-item">
+                                    <input placeholder="Search" type="search" class="gn-search">
+                                    <a class="gn-icon gn-icon-search"><span>Search</span></a>
+                                </li>
+                                <li>
+                                    <a class="gn-icon gn-icon-download">Downloads</a>
+                                    <ul class="gn-submenu">
+                                        <li><a class="gn-icon gn-icon-illustrator">Vector Illustrations</a></li>
+                                        <li><a class="gn-icon gn-icon-photoshop">Photoshop files</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="gn-icon gn-icon-cog">Settings</a></li>
+                                <li><a class="gn-icon gn-icon-help">Help</a></li>
+                                <li>
+                                    <a class="gn-icon gn-icon-archive">Archives</a>
+                                    <ul class="gn-submenu">
+                                        <li><a class="gn-icon gn-icon-article">Articles</a></li>
+                                        <li><a class="gn-icon gn-icon-pictures">Images</a></li>
+                                        <li><a class="gn-icon gn-icon-videos">Videos</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </li>
+                <li><a href="http://tympanus.net/codrops">Codrops</a></li>
+                <li><a class="codrops-icon codrops-icon-prev" href=""><span>Previous Demo</span></a></li>
+                <li><a class="codrops-icon codrops-icon-drop" href=""><span>Back to the Codrops Article</span></a></li>
+            </ul>
+            <main>
+       
 
-</head>
+                <div class="well col-sm-3" style="height:auto; margin-top:80px">
+                     <div class= "mainTitle col-sm-12">Meteo temps réel</div>
+                    <div class="  col-xl-12 col-xs-12 col-md-12 col-lg-6  jauge"  id="humidite"></div>
+                    <div class="   col-xl-12 col-xs-12 col-md-12 col-lg-6 jauge" id="pluie"></div>
+                    <div class="  col-xl-12 col-xs-12  col-md-12 col-lg-6 jauge" id="luminosite"></div>
+                    <div class=" col-xl-12 col-xs-12  col-md-12 col-lg-6 jauge" id="temperatureExterieure"></div>
+                    <div class="solidJauge  col-md-12" id="deltaTemperature" ></div>
+                </div>
+                <div class="well col-sm-4" style="margin-top:80px;height:500px; padding:5px;">
+                    <button id="Resistance">Resistance chauffante</button></br></br>
+                    <button id="Alarme">Alarme</button></br></br>
+                    <button id="Toit">Toit</button></br></br>
+                </div>
 
-<body>
+                <div class="well col-sm-4" style="margin-top:80px; height:auto; padding:5px;">
+                    <div class="graphique" id="graphePression" ></div>
+                    <div class="graphique" id="grapheHumidite" ></div>
+                    <div class="graphique" id="grapheTemperatureExterieure" ></div>
+                    <div class="graphique" id="grapheDetectionEau"></div>
+                    <div class="graphique" id="grapheLuminosite"></div>
+                </div>
 
+         
+            </main>
 
-<!-- Affichage des jauges temps réel  -->
-<div class="container">
-    <div class="row">
-        <div class="jauge" id="humidite"></div>
-        <div class="jauge" id="pluie"></div>
-        <div class="jauge" id="pression"></div>
-        <div class="jauge" id="luminosite"></div>
-    </div>
-    <div class="row">
-        <div class="jauge" id="temperatureExterieure"></div>
-        <div class="solidJauge" id="deltaTemperature" ></div>
-                <div class="col-xs-2">
-            <?php
-      //      $Sat24 = new Sat24();
-       //     echo $Sat24->afficheLune();
-            
-            ?>
         </div>
+        
+        <script src="template/js/classie.js"></script>
+        <script src="template/js/gnmenu.js"></script>
+        <script>
+                new gnMenu( document.getElementById( 'gn-menu' ) );
+        </script>
+        
+        
     </div>
-    <div class="row">
-        <div class="col-xs-8">
-            <?php
-    //        echo $Sat24->Date();
-            ?>
-        </div>
-        <div class="col-xs-2"><h4>Interprétation :</h4></br>
-            Alertes : </br>
-            Humidité : 100%  : Fort risque de pluie </br>
-            Pression : Baisse de 2HPa dans les 2 dernières heures ! </br>        
-        </div>
-    </div>
-</div>
-
-</br> 
-______________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-</br></br>
-    
-<h1> Contrôle de l'observatoire </h1></br>
-</br>
-
-
-<button id="TensionTelescope">Mise sous tension télescope</button></br></br>
-<button id="Resistance">Resistance chauffante</button></br></br>
-<button id="Alarme">Alarme</button></br></br>
-<button id="Toit">Toit</button></br></br>
-</br>
-
-<div style="height:400px;width:400px;background-color:grey">VUE 3D du télescope</div>
-</br></br></br></br>______________________________________________________________________________________________________________________________________________________________________________________________________________
-
-</br></br>
-<h1>Affichage des graphiques journaliers</h1>
-<?php
-// Au chargement, on va faire la requete SQL qui va remplir correctement le json avec les dernieres 24h de données
-$MajJson = new Json(0,"json/meteo24h.json");
-$MajJson->save24h();
-
-?>
-<div class="graphique" id="graphePression" ></div>
- <div class="graphique" id="grapheHumidite" ></div>
-<div class="graphique" id="grapheTemperatureExterieure" ></div>
-<div class="graphique" id="grapheDetectionEau"></div>
-<div class="graphique" id="grapheLuminosite"></div>
-
-</br></br></br></br>______________________________________________________________________________________________________________________________________________________________________________________________________________
-
-<h1> Affichage historique graphiques </h1>
-
-</br></br>
-
-
-
-
-</body>
+    </body>
 </html>
