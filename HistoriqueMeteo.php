e14522d3afa9131cd7079eda17786e32471a9447
MaximeS <maxime.simet@yahoo.fr>
2016-05-24 17:36:28 +0200

    teest

--- HistoriqueMeteo.php
+++ HistoriqueMeteo.php
@@ -4,7 +4,7 @@
         <title> Station Météo</title>
         <meta name="robots" content="noindex">
         <meta charset="UTF-8" />
-        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="initial-scale=2.0, user-scalable=no" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <link rel="shortcut icon" href="../favicon.ico">
